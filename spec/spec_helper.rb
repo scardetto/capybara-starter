5dfa92064c0fab9af8d3aa0e578a630964dfb00f
Rob Scaduto <rob@thirdwave.it>
2018-01-10 16:23:07 -0500

    Updated dependencies to use headless chrome

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,35 +1,48 @@
 require 'capybara/rspec'
-require 'capybara/poltergeist'
-require 'selenium-webdriver'
 require 'dotenv'
 
 # Load env vars from .env file
 Dotenv.load
 
-# Register Chrome driver
-Capybara.register_driver :selenium_chrome do |app|
-  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+# Configure Selenium WebDriver
+require 'selenium/webdriver'
+driver = :selenium
+browser = :chrome
+options = {}
+
+Capybara.register_driver driver do |app|
+  driver_options = {browser: browser}.merge(options)
+  Capybara::Selenium::Driver.new(app, driver_options)
 end
 
-# Use PhantomJS by default
-driver = :poltergeist
+# Configure Chrome Browser
+driver = :chrome
+browser = :chrome
+options = {}
 
-# Determine the driver based on ENV var
-case (ENV['DRIVER'] && ENV['DRIVER'].upcase)
-when 'FIREFOX', 'FF'
-  driver = :selenium
-when 'CHROME'
-  driver = :selenium_chrome
+Capybara.register_driver driver do |app|
+  driver_options = {browser: browser}.merge(options)
+  Capybara::Selenium::Driver.new(app, driver_options)
 end
-Capybara.default_driver = driver
 
-# Check for brew cask Firefox
-if (/darwin/ =~ RUBY_PLATFORM) != nil # Mac OSX
-  brew_cask_path = "#{ENV['HOME']}/Applications/Firefox.app"
-  if File.symlink? brew_cask_path
-    Selenium::WebDriver::Firefox::Binary.path = "#{File.readlink(brew_cask_path)}/Contents/MacOS/firefox-bin"
-  end
+# Configure Headless Chrome
+driver = :headless_chrome
+browser = :chrome
+driver_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+  chromeOptions: {
+    args: %w(headless disable-gpu no-sandbox)
+  }
+)
+
+Capybara.register_driver driver do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: browser,
+    desired_capabilities: driver_capabilities)
 end
+
+Capybara.javascript_driver = :headless_chrome
+Capybara.default_driver = :headless_chrome
 
 # Require all support files
 Dir["./spec/support/*.rb"].each {|file| require file }
